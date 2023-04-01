93e758119be3d12bb1fc5ee182ddc0b19568c280
Kushal <kushalbhana2050@gmail.com>
2023-04-01 21:48:47 +0530

    Basic features finished-1

--- app/consumers.py
+++ app/consumers.py
@@ -78,9 +78,9 @@ class MySyncConsumer(SyncConsumer):
 
 class MyAsyncConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
-        print('Websocket connected...', event)
-        print('Channel Layer...', self.channel_layer) #get default channel layer from the project
-        print('Channel Name...', self.channel_name) #get default channel name from the project
+        # print('Websocket connected...', event)
+        # print('Channel Layer...', self.channel_layer) #get default channel layer from the project
+        # print('Channel Name...', self.channel_name) #get default channel name from the project
         self.group_name= self.scope['url_route']['kwargs']['groupname']
         self.channel_layer.group_add(
             self.group_name,
@@ -97,8 +97,8 @@ class MyAsyncConsumer(AsyncConsumer):
         })
 
     async def websocket_receive(self, event):
-        print("Message Recieved from client", event['text']) 
-        print("Type of Message Recieved from client", type(event['text'])) 
+        # print("Message Recieved from client", event['text']) 
+        # print("Type of Message Recieved from client", type(event['text'])) 
         try:
             data= json.loads(event['text'])
             group= await database_sync_to_async(Group.objects.get)(name= self.group_name)
@@ -121,7 +121,7 @@ class MyAsyncConsumer(AsyncConsumer):
         )
 
     async def chat_message(self, event):
-        print('Event...', event)
+        # print('Event...', event)
         await self.send({
             'type': 'websocket.send',
             'text': event['message']
@@ -129,9 +129,9 @@ class MyAsyncConsumer(AsyncConsumer):
  
 
     async def websocket_disconnect(self, event):
-        print("Websocket Disconnected", event)
-        print('Channel Layer...', self.channel_layer) #get default channel layer from the project
-        print('Channel Name...', self.channel_name) #get default channel name from the project
+        # print("Websocket Disconnected", event)
+        # print('Channel Layer...', self.channel_layer) #get default channel layer from the project
+        # print('Channel Name...', self.channel_name) #get default channel name from the project
         await self.channel_layer.group_discard(
             'programmers',
              self.channel_name
